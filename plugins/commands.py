087dadcb0336e8862dbea14a9e4f24d661c74b13
hijas-hue <81763350+hijas-hue@users.noreply.github.com>
2021-04-02 12:03:57 +0530

    update commands.py
    New command changed ,please accept bro.

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "⭕️ JOIN OUR GROUP ⭕️", url="https://t.me/searchit00")
                     ]
                 ]
             ),
